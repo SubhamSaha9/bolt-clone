26467d45114cd4f61a16c4ec028502698259ae14
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-22 21:34:53 +0530

    initialize project with Vite, React, and TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <div className="h-screen">
+      <Button variant="outline">jello</Button>
+      <div className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+        architecto. Optio omnis modi doloribus quae, quod doloremque! Alias,
+        saepe dignissimos enim repellat suscipit, asperiores quam officiis,
+        quaerat optio ut sapiente?
+      </div>
+    </div>
+  );
+}
+
+export default App;
