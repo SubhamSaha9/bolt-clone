17ba5792fd3e0e83572c8b7af6459f0f3c339932
SubhamSaha9 <sahasubham7478@gmail.com>
2025-02-25 22:01:00 +0530

    add profile card to sidebar

---
+++ src/components/common/Profile.tsx
@@ -0,0 +1,126 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  Github,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+} from "lucide-react";
+import { logout } from "../../slice/authSlice";
+import { useNavigate } from "react-router-dom";
+import { setOpenSheet } from "../../slice/actionSlice";
+
+const Profile = () => {
+  const { user } = useSelector((state: any) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    dispatch(logout());
+    dispatch(setOpenSheet(false));
+    navigate("/");
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="flex gap-2 bg-gray-900 hover:bg-gray-800 shadow-sm p-2 rounded cursor-pointer border">
+          <img src={user.image} alt="profile" className="rounded h-8" />
+          <div className="flex flex-col items-start justify-center text-xs">
+            <b>
+              {user.firstName} {user.lastName}
+            </b>
+            <p className="text-gray-400 line-clamp-1">{user.email}</p>
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="right" sideOffset={10} className="w-60">
+        <DropdownMenuLabel>
+          <div className="flex gap-2 shadow-sm p-2 rounded cursor-pointer">
+            <img src={user.image} alt="profile" className="rounded h-8" />
+            <div className="flex flex-col items-start justify-center text-xs">
+              <b>
+                {user.firstName} {user.lastName}
+              </b>
+              <p className="text-gray-500 18">{user.email} </p>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings />
+            <span>Settings</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <UserPlus />
+              <span>Invite users</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>
+                  <Mail />
+                  <span>Email</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <MessageSquare />
+                  <span>Message</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <PlusCircle />
+                  <span>More...</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            <Plus />
+            <span>New Chat</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Github />
+          <span>GitHub</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LifeBuoy />
+          <span>Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogOut}>
+          <LogOut />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Profile;
