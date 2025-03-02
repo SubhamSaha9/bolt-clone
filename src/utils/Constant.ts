498189e089b2126afc64f75397fc90f15fa3b30b
SubhamSaha9 <sahasubham7478@gmail.com>
2025-03-03 02:24:41 +0530

    add utils file

---
+++ src/utils/Constant.ts
@@ -0,0 +1,16 @@
+import { BadgeHelp,  Settings, Wallet } from "lucide-react";
+
+export const SideBarFooter = [
+    {
+        name:"Settings",
+        icon:Settings,
+    },
+    {
+        name:"Help Center",
+        icon:BadgeHelp,
+    },
+    {
+        name:"My Subscription",
+        icon:Wallet,
+    },
+]
